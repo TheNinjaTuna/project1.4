5f18e156c8f2fb14edc5c8f085e7c9fe53c765f2
TheNinjaTuna <j.viswat@st.hanze.nl>
2019-05-27 13:45:12 +0200

    Base code upload

---
+++ classes/Artikel.java
@@ -0,0 +1,33 @@
+class Artikel
+{
+    private String naam;
+    private float prijs;
+
+    public Artikel() {
+
+    }
+
+    public Artikel(String naam, float prijs) {
+
+    }
+
+    /// Getters en setters van naam
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+    /// Getters en setters van prijs
+
+    public float getPrijs() {
+        return prijs;
+    }
+
+    public void setPrijs(float prijs) {
+        this.prijs = prijs;
+    }
+
+}
---
+++ classes/Datum.java
@@ -0,0 +1,78 @@
+public class Datum {
+
+	private int dag;
+	private int maand;
+	private int jaar;
+
+	/**
+	 * Parameter-loze constructor
+	 */
+	public Datum() {
+		this.dag = 0;
+		this.maand = 0;
+		this.jaar = 0;
+	}
+
+	/**
+	 * Constructor
+	 */
+	public Datum(int dag, int maand, int jaar) {
+		this.dag = dag;
+		this.maand = maand;
+		this.jaar = jaar;
+	}
+
+	public boolean bestaatDatum(int dag, int maand, int jaar){
+		// TODO
+		return false;
+	}
+	
+	/**
+	 * Getter voor Sting weergave van datum
+	 * @return Geboortedatum
+	 */
+	public String getDatumAsString() {
+		// TODO
+		return "";
+		}
+
+		// Getters en setters voor instantievariabelen
+
+	public int getDag() {
+		return dag;
+	}
+
+	public void setDag(int dag) {
+		this.dag = dag;
+	}
+
+	public int getMaand() {
+		return maand;
+	}
+
+	public void setMaand(int maand) {
+		this.maand = maand;
+	}
+
+	public int getJaar() {
+		return jaar;
+	}
+
+	public void setJaar(int jaar) {
+		this.jaar = jaar;
+	}
+
+	private boolean	bestaatDatum() {
+		if (dag <1 || >31|| (maand <1 || >12) ||(jaar <1900 || >2100)){
+			return false;
+		}
+		elseif ((maand != 1 && maand != 3 && maand != 5 && maand != 7 && maand != 8 && maand != 10 && maand != 12)  &&  (dag > 30 ) && (!jaar % 4 == 0))
+	{return false;}
+		else
+		{
+			return true;
+		}
+	}
+
+
+}
